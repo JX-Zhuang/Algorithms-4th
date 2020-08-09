3743211e54180cbb788fb121ca33f1704867d39f
jxzhuang <jxzhuang@zhuangjiaxiangdeMacBook-Pro.local>
2020-08-09 16:43:47 +0800

    update

--- Hash/SeparateChainingHashST.js
+++ Hash/SeparateChainingHashST.js
@@ -15,24 +15,53 @@ class SeparateChainingHashST {
     put(key, val) {
         this.st[this.hash(key)][key] = val;
     }
-    delete(key){
+    delete(key) {
         delete this.st[this.hash(key)][key];
     }
 }
-var st = new SeparateChainingHashST(100);
-st.put('name','zjx');
-st.put('age',12);
-st.put('1234','4321');
-console.log(st.get('name'),st.get('age'),st.get('1234'));
-st.delete('1234')
-console.log(st.get('name'),st.get('age'),st.get('1234'));
-class LinearProbingHashST{
+function testSeparateChainingHashST() {
+    var st = new SeparateChainingHashST(100);
+    st.put('name', 'zjx');
+    st.put('age', 12);
+    st.put('1234', '4321');
+    console.log(st.get('name'), st.get('age'), st.get('1234'));
+    st.delete('1234')
+    console.log(st.get('name'), st.get('age'), st.get('1234'));
+}
+class LinearProbingHashST {
     keys = [];
     vals = [];
-    M = 16;
-    N;
-    constructor(){
+    M = 30;
+    constructor() {
         this.keys = new Array(this.M);
         this.vals = new Array(this.M);
     }
+    hash(key) {
+        return String(key).length % this.M;
+    }
+    put(key, val) {
+        let i;
+        for (i = this.hash(key); this.keys[i] != undefined; i = (i + 1) % this.M) {
+            if (this.keys[i] === key) {
+                this.vals[i] = val;
+            }
+        }
+        this.keys[i] = key;
+        this.vals[i] = val;
+        return;
+    }
+    get(key) {
+        for (var i = this.hash(key); this.keys[i] != undefined; i = (i + 1) % this.M) {
+            if (this.keys[i] === key) return this.vals[i];
+        }
+        return null;
+    }
 }
+function testLinearProbingHashST() {
+    var st = new LinearProbingHashST();
+    st.put('name', 'zjx');
+    st.put('age', 12);
+    st.put('1234', '4321');
+    console.log(st.get('name'), st.get('age'), st.get('1234'));
+}
+testLinearProbingHashST();
